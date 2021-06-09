7e8c7bc4d87b263adaa6389dc6a4aef51d2ee76b
Neutron-Pro <neutron@neutronstars.fr>
2021-06-09 21:13:36 +0200

    Fix EnumType

--- src/Types/EnumType.php
+++ src/Types/EnumType.php
@@ -9,22 +9,15 @@ use Doctrine\DBAL\Types\Type;
 use Doctrine\DBAL\Types\Types;
 use NeutronStars\Enum\Enum;
 
-class EnumType extends Type
+abstract class EnumType extends Type
 {
-    public const NS_ENUM = 'ns_enum';
-
-    public function getName(): string
-    {
-        return self::NS_ENUM;
-    }
-
     public function getSQLDeclaration(array $column, AbstractPlatform $platform): string
     {
         return Types::STRING;
     }
 
-    public function convertToPHPValue($value, AbstractPlatform $platform): ?string
+    public function convertToDatabaseValue($value, AbstractPlatform $platform): ?string
     {
-        return $value instanceof Enum ? (string) $value : null;
+        return ($value instanceof Enum) ? (string) $value : null;
     }
 }
